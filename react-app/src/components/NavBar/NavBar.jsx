c786f504c78041fd911a28ce4f688ed861673a2e
Brian Jian Zhao <jian10au@gmail.com>
2019-12-17 06:26:06 +1100

    fix className

--- react-app/src/components/NavBar/NavBar.jsx
+++ react-app/src/components/NavBar/NavBar.jsx
@@ -7,17 +7,17 @@ const NavBar = () => {
 
     return(
         <nav className={styles.NavBar}>
-            <div class={styles.LogoSection}>
+            <div className={styles.LogoSection}>
                 <img src={logo} class={styles.Logo} alt="logo" />
-                <h2 class={styles.LogoText}>datical</h2>
-                <span class={styles.TagLine}>Learn code and get practical</span>
+                <h2 className={styles.LogoText}>datical</h2>
+                <span className={styles.TagLine}>Learn code and get practical</span>
             </div>
             <ul className='links'>
-                <li><NavLink class={styles.NavLink} to={"/candidates/:id/attempts"}>View my submissions</NavLink></li>
-                <li><NavLink class={styles.NavLink} to={"/challenges"}>Browse challenges</NavLink></li>
-                <li><NavLink class={styles.NavLink} to={"/candidates/:id"}>Profile</NavLink></li>
-                <li><NavLink class={styles.NavLink} to={"/signup"}>Sign In</NavLink></li>
-                <li><NavLink class={styles.NavLink} to={"/signup"}>Sign Out</NavLink></li>       
+                <li><NavLink className={styles.NavLink} to={"/candidates/:id/attempts"}>View my submissions</NavLink></li>
+                <li><NavLink className={styles.NavLink} to={"/challenges"}>Browse challenges</NavLink></li>
+                <li><NavLink className={styles.NavLink} to={"/candidates/:id"}>Profile</NavLink></li>
+                <li><NavLink className={styles.NavLink} to={"/signup"}>Sign In</NavLink></li>
+                <li><NavLink className={styles.NavLink} to={"/signup"}>Sign Out</NavLink></li>       
             </ul>
         </nav>
     )
