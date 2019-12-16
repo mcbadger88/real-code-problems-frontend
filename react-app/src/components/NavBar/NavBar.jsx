e150ed036dfd5ed2e855c905f2a68b0320364dcc
Brian Jian Zhao <jian10au@gmail.com>
2019-12-16 23:24:56 +1100

    nav-bar polished

--- react-app/src/components/NavBar/NavBar.jsx
+++ react-app/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import styles from '../NavBar/NavBarStyles.module.css'
+import logo from '../../assets/SiteLogo.svg'
 
 const NavBar = () => {
 
     return(
         <nav className={styles.NavBar}>
+            <div class={styles.LogoSection}>
+                <img src={logo} class={styles.Logo} alt="logo" />
+                <h2 class={styles.LogoText}>datical</h2>
+                <span class={styles.TagLine}>Learn code and get practical</span>
+            </div>
             <ul className='links'>
                 <li><NavLink class={styles.NavLink} to={"/candidates/:id/attempts"}>View my submissions</NavLink></li>
                 <li><NavLink class={styles.NavLink} to={"/challenges"}>Browse challenges</NavLink></li>
