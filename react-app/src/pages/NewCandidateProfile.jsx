5eea7c8743d6c2723f888241abd7924f484e0c7e
Brian Jian Zhao <jian10au@gmail.com>
2020-01-09 16:00:16 +1100

    page boilplate created

---
+++ react-app/src/pages/NewCandidateProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NewChandidateProfite = () => {
+
+    return(
+        <h1>This is the page we use to create new candidate</h1>
+    )
+
+}
+
+export default NewChandidateProfite
+
+
+
+
+
+
