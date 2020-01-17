726376e64e518c1f150a2562467af0bb2cb042ec
tayaaaa <lacey.taya@gmail.com>
2020-01-17 17:28:35 +1100

    adds file upload button, allowing us to upload zip files to s3

--- react-app/src/components/ChallengeButtonArea/ChallengeButtonArea.jsx
+++ react-app/src/components/ChallengeButtonArea/ChallengeButtonArea.jsx
@@ -3,6 +3,8 @@ import styles from './ChallengeButtonArea.module.css';
 import {apiCreateAttempt, apiSubmitAttempt} from '../../api/attempt'
 import {apiSetChallengeEnabled} from '../../api/challenge'
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
+
 
 const ChallengeButtonArea = ({challenge, attemptStatus, onChallengeStatusChange}) => {
     const [dockerFileLocation, setDockerFileLocation] = useState(null)
@@ -20,6 +22,19 @@ const ChallengeButtonArea = ({challenge, attemptStatus, onChallengeStatusChange}
         onChallengeStatusChange()
         // const started = await apiIsActiveAttempt(challenge._id)
         // setChallengeStarted(!started)
+    }
+
+    const saveFile = async (e) => {
+        e.preventDefault()
+        const form = document.forms.namedItem('file-upload')
+        const data = new FormData(form)
+
+        try{
+            const response = await Axios.post('http://localhost:5000/candidate/5df865457947111ec1a81cad/attempts/5df865457947111ec1a81cb1/upload', data)
+
+        }catch(err) {
+            console.log(err)
+        }
     }
 
     // in response to disable challenge, create disableChallene() and call the challenge update route
@@ -42,6 +57,13 @@ const ChallengeButtonArea = ({challenge, attemptStatus, onChallengeStatusChange}
                         type="submit" 
                         >download zip file
                         </button>
+
+                        {/* Hi! I know this looks messy, but it works!! we can clean but the styling but I'm tired and I'm gonna leave it here and go home. This means that we can upload zip files to s3 and send Micheal the URL to download them directly from amazon. Cool! */}
+                        <form encType="multipart/form-data" name="file-upload" onSubmit={saveFile}>
+                            <input type="file" name="image" id="image"/>
+                            <button type="submit"> Submit </button>
+                        </form>
+
                         <button 
                             onClick={() => { apiSubmitAttempt()}}
                             type="submit"
