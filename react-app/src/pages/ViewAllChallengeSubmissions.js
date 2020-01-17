fb40a67147b576311cc2700ed16ab31e2d29eadf
tayaaaa <lacey.taya@gmail.com>
2020-01-17 12:27:52 +1100

    creates page to view all attempts made for a single challenge

--- react-app/src/pages/ViewAllChallengeSubmissions.js
+++ react-app/src/pages/ViewAllChallengeSubmissions.js
@@ -1,9 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import AttemptCard from '../components/AttemptCard/AttemptCard';
+import styles from './MyChallengeSubmissions.module.css';
+import AttemptTableHeadings from '../components/AttemptTableHeadings/AttemptTableHeadings';
+import { getAllAttemptsForChallenge } from '../api/attempt';
 
-const ViewAllChallenges = () => {
+
+const ViewAllChallengeSubmissions = ({challengeID}) => {
+    const [submissions, setSubmissions] = useState(null);
+
+    useEffect(() => {
+        const getAttempts = async() => {
+            const subs = await getAllAttemptsForChallenge(challengeID)
+            console.log(subs)
+            setSubmissions(subs)
+        }
+        getAttempts()
+    }, [])
+
     return (
-        <h1>VIEW CHALLENGE SUBMISSIONS HERE !</h1>
+        <>
+        <NavBar/>
+        <div className={styles.tableContainer}>
+            <h1>All Challenge Submissions: </h1>
+
+            <table>
+                <AttemptTableHeadings/>
+                <tbody>
+                    {submissions ? submissions.map(submission =>     
+                        // When submissions exist, successfully create a new table row and render an attempt card for each submission.
+                            <AttemptCard deleteAvailable={false} status={submission.status} challenge={submission.challenge_id.title} dateSubmitted={submission.updatedAt} candidate={submission.candidate_id} challengeID={submission.challenge_id._id} attemptID={submission._id} username={submission.candidate_id.username}/>) : <tr> <p> Loading... </p></tr>}
+                </tbody>
+            </table>
+
+        </div>
+        </>
     )
 }
 
-export default ViewAllChallenges
+export default ViewAllChallengeSubmissions
